6c0c5d7a88facfb2f4d80792e6b9d5fe6eb8d578
Rajan Poudel <raajan.works@gmail.com>
2025-04-26 22:12:08 +0545

    Add files via upload

---
+++ form.php
@@ -0,0 +1,23 @@
+<?php
+if (isset($_POST['submit'])) {
+    $name = $_POST["username"];
+    $ps = $_POST["password"];
+    $rps = $_POST["psw-repeat"];
+}
+
+
+if($ps != $rps) {
+    echo '<p style="color:red; text-align:center; font-weight:bold;">Passwords do not match!</p>';
+    exit;
+} else {
+    echo '<h2 style="color:#28a745;text-align:center;display:flex; justify-content:center; align-items:center; height:100vh; font-weight:bold;">Form Submitted Successfully!</h2>';
+}
+
+$hashed_password = password_hash($ps, PASSWORD_DEFAULT);
+
+$filename = "data.txt";
+$fp = fopen($filename, "a") or die("Unable to open file!");
+$str = "Name: $name, Password: $hashed_password\n"; // Save hashed password
+fwrite($fp, $str);
+fclose($fp);
+?>
